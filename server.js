6d2df2d5029a4e1808b5667431f5d29d6f0b21a5
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-08-15 16:24:40 +0000

    adding .gitignore and some tutorials

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const http = require("http");
 
 const port = 8081;
 
+const todos_List = ["purchase", "code", "learn"];
+
 http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type": "text/html"});
-    res.write("<h2>Hey your server is running :)</h2>");
+    // res.writeHead(200,{"Content-Type": "text/html"});
+    // res.write("<h2>Hey your server is running :) Again hello</h2>");
+    
+    const {url, method} = req;
+    console.log(method, url);
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            res.writeHead(200);
+            res.write(todos_List.toString());
+        }
+    }
     res.end();
 })
 .listen(port, ()=>{
     console.log(`NodeJs Server is listening at Port : ${port}`);
-});
+}); 
