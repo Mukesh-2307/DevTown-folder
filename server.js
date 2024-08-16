dfee45da8c3f6d006107081910a07b9a1ed6bbd4
Mukesh Kumar Dalai <99747839+Mukesh-2307@users.noreply.github.com>
2024-08-16 17:28:17 +0000

    hands-on different methods to comm with node server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const http = require("http");
 
 const port = 8081;
@@ -13,12 +14,64 @@ http.createServer((req,res)=>{
 
     if(url === "/todos"){
         if(method === "GET"){
-            res.writeHead(200);
+            res.writeHead(200,{"Content-Type": "text/html"});
             res.write(todos_List.toString());
         }
+        else if(method === "POST"){
+            // res.writeHead(200,{"content-type": "text/html"});
+            // res.write("<h1>you are inside post request</h1>");
+
+            let body = "";
+            req.on("error",(err)=>{
+                console.log(err);
+            }).on("data",(chunk)=>{
+                body += chunk;
+                // console.log(chunk);
+            }).on("end",()=>{
+                body = JSON.parse(body);
+                let newTodos = todos_List;
+                newTodos.push(body.item);
+                console.log(newTodos);
+                // console.log("data: ",body);
+            })
+        }
+        else if(method === "DELETE"){
+            let body = "";
+            req.on('error',(err)=>{
+                console.log(err);
+            }).on("data",(chunk)=>{
+                body += chunk;
+                // console.log(chunk);
+            }).on("end",()=>{
+                body = JSON.parse(body);
+                let newTodos = todos_List;
+                let deletedItem;
+                // deletedItem = newTodos.pop[1];
+                // deletedItem.pop(body.item);
+                deletedItem = body.item;
+                for(let i = 0; i < newTodos.length;i++){
+                    // we can also use ".find" method to find the element to be deleted 
+                    if(newTodos[i] === deletedItem){
+                        newTodos.splice(i,1);
+                        console.error(`the output of the delete ops ${newTodos}`);
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+                console.log(deletedItem);
+            })
+        }
+        else {
+            res.writeHead(501);
+        }
+    }
+    else{
+        res.writeHead(404);
     }
     res.end();
 })
 .listen(port, ()=>{
     console.log(`NodeJs Server is listening at Port : ${port}`);
-}); 
+});
